c6bd76236b1484a999595a574bd7f64ce124810d
Reinoud Schuijers <reinoud@reinoud-jules.nl>
2015-10-16 14:44:34 +0200

    Fix datepicker JS-error when it's not used

--- js/main.js
+++ js/main.js
@@ -203,21 +203,22 @@ $(document).ready(function() {
 	});
 	
 	
-	
-	var today = new Date();
-	$("div#datepicker").datepicker({
-		minDate: today,
-		maxDate: 7,
-		beforeShowDay: function(date) {
-	        var day = date.getDay();
-	        return [(day != 0), ''];
-	    },
-	    onSelect: function(dateTxt, inst) {
-		    $("input#afleverdag").val(dateTxt);
-	    }
-	}, $.datepicker.regional['nl']);
-	
-	$("div#datepicker").datepicker("setDate", 1);
+	if($("div#datepicker").length != 0) {
+		var today = new Date();
+		$("div#datepicker").datepicker({
+			minDate: today,
+			maxDate: 7,
+			beforeShowDay: function(date) {
+		        var day = date.getDay();
+		        return [(day != 0), ''];
+		    },
+		    onSelect: function(dateTxt, inst) {
+			    $("input#afleverdag").val(dateTxt);
+		    }
+		}, $.datepicker.regional['nl']);
+		
+		$("div#datepicker").datepicker("setDate", 1);
+	}
 	
 	/* ===================================== */
 	/* >>>>>>>> PROD DETAIL GALLERY
